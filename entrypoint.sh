28139b852fefa2ece14f73cb2d49b77bfe8e9ccd
SHARAD RAJ <36638057+sharadcodes@users.noreply.github.com>
2020-04-06 23:42:50 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,33 +1,29 @@
 #!/bin/sh -l
-
 echo "###############################################"
 echo "################## mkdir  #####################"
 echo "###############################################"
 # Make directory for storage of converted font files
 mkdir "$2"
-
 echo "###############################################"
 echo "################ Setting up  ##################"
 echo "###############################################"
 # Setup
 git clone https://github.com/bdusell/webfont-generator.git
+wget https://raw.githubusercontent.com/sharadcodes/font-typekit-generator-action/master/gen.py
 cd webfont-generator
 ./setup # Fetch and build third-party libraries
-
 echo "###############################################"
 echo "##################### cd  #####################"
 echo "###############################################"
 # change dir
-tree
 cd ..
 echo "###############################################"
 echo "################ Conversion  ##################"
 echo "###############################################"
-tree
 python3 gen.py $1 $2
-
 echo "###############################################"
 echo "################ Cleaning repo ################"
 echo "###############################################"
 rm -rf ./webfont-generator
+rm -rf ./gen.py
 tree
